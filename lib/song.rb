3e2bc788aef18e704fe0cbd561b2645fb5bb5bae
Kimberly Aviles <01010110.v@gmail.com>
2019-02-25 23:03:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,9 @@ class Song # create songs & send artist name STRING to ARTIST CLASS
     @artist = artist
   end
 
-  # def artist_find_or_create(name) ######DOES THIS NEED TO EXIST OR CAN I CALL STRAIGHT FROM ARTIST CLASS????
-  #   Artist.find_or_create_by_name(name)
-  # end
+  def artist_find_or_create(name) ######DOES THIS NEED TO EXIST OR CAN I CALL STRAIGHT FROM ARTIST CLASS????
+    Artist.find_or_create_by_name(name)
+  end
 
   def self.new_by_filename(filename)
     #parse the file for the song and artist
