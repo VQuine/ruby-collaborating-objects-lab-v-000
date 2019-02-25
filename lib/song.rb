1cb4d90a61072edd9b0e715fbaa23bd121f11d2c
Kimberly Aviles <01010110.v@gmail.com>
2019-02-25 23:41:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,11 +7,6 @@ class Song # create songs & send artist name STRING to ARTIST CLASS
     @artist = artist
   end
 
-  def artist_find_or_create(name) ######DOES THIS NEED TO EXIST OR CAN I CALL STRAIGHT FROM ARTIST CLASS????
-    obj = Artist.find_or_create_by_name(name)
-    obj.save
-  end
-
   def self.new_by_filename(filename)
     #parse the file for the song and artist
     file_split = filename.split(/-/)
@@ -22,7 +17,7 @@ class Song # create songs & send artist name STRING to ARTIST CLASS
     song_object = Song.new(song_name)
 
     #associate new song instance with an artist
-    song_object.artist = song_object.artist_find_or_create(artist_name)
+    artist_object = Artist.find_or_create_by_name(artist_name)
 binding.pry
 
     #return new song instance
