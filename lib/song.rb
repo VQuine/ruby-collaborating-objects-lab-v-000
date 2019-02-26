ff2773303fabe4ee44ca86f847c45b90c97be7e8
Kimberly Aviles <01010110.v@gmail.com>
2019-02-26 00:25:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,12 +18,8 @@ class Song # create songs & send artist name STRING to ARTIST CLASS
 
     #associate new song instance with an artist
     artist_object = Artist.find_or_create_by_name(artist_name)
-
     artist_object.save
-
-
     song_object.artist = artist_object
-
     artist_object.add_song(song_name)
 
     #return new song instance
